0708051b5dc4443dabe61ecb26d614dacb1a8d85
nikolaevaElizaveta <liza-nikolaeva-04@mail.ru>
2022-11-14 15:24:20 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import javax.naming.NoPermissionException;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.channels.NonWritableChannelException;
+import java.nio.file.NoSuchFileException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        try(FileInputStream in = new FileInputStream("input.txt");
+            FileOutputStream out = new FileOutputStream("output.txt"))
+        {
+            byte[] buffer = new byte[in.available()];
+            in.read(buffer, 0, buffer.length);
+            String inp= Arrays.toString(buffer);
+            String[] numbers=inp.split(" ");
+            int first=Integer.parseInt(numbers[0]);
+            int second=Integer.parseInt(numbers[1]);
+            if (second==0){
+                System.out.println("div y zero");
+                System.exit(0);
+            }
+            out.write(first/second);
+
+        } catch (FileNotFoundException exOne){
+            System.out.println("No such file");
+        } catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }catch (NumberFormatException e){
+            System.out.println("mistake");
+        }
+    }
+}
